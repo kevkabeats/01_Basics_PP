faafaaa7d89ee1a6cddbf77cfeb2354a4d6345ac
Kevin Belsch <kev.b95@hotmail.com>
2025-11-06 11:04:48 +0000

    Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -13,11 +13,54 @@
 //console.log("Moin!");
 //console.log(Moin)
 
+
+
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | Assingment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
+//let firstName; // Deklaration (Definition)
+//console.log(firstName); // Ausgabe
+//firstName = "Max"; // Wertzuweisung | Assingment
+//console.log(firstName); // Ausgabe
+//let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
+
+
+
+/* Wertzuweisung II + Datentypen */
+
+/* let test;
+
+/* undefined */
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
+
+//test = "hi"; // string
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
+
+//test = 2; // number
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
+
+//test = true; // boolean
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
+
+
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "Hi!" // Deklaration + Wertzuweisung gemeinsam !
+// test2 = "hello"; //Laufzeit-Fahler 
+console.log("Inhalt:" + test2); //Ausgabe
