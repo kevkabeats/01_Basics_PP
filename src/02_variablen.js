3a885695661372d5142e87b796ada69114aa4ec9
Kevin Belsch <kev.b95@hotmail.com>
2025-11-06 10:12:25 +0000

    Deklaration

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -9,3 +9,15 @@
  */
 
 /***** Variablen 01 *******/
+
+//console.log("Moin!");
+//console.log(Moin)
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | Assingment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
