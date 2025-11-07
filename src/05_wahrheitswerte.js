74279af0f0938a49d29176174b284acef67d9bb4
Kevin Belsch <kev.b95@hotmail.com>
2025-11-07 11:21:10 +0000

    05_Iteration 3

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,7 +3,7 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 1980;
+const birthYearJohn = 1990;
 const birthYearMark = 1990;
 
 // Berechnung Alter Iteration 1
@@ -12,17 +12,31 @@ ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
  */
 // Berechnung Alter Iteration 2
-let date = new Date();
+/* let date = new Date();
 let year = date.getFullYear();
 
 console.log("Datum: " + date);
 console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
-ageMark = year - birthYearMark;
+ageMark = year - birthYearMark; */
 
-//let year = 
+// Berechnung Alter Iteration 3
+let year = new Date().getFullYear();
+console.log("Jahr: " + year);
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+// Deklaration
+let isJohnOlder;
+
+// Vergleich
+isJohnOlder = ageJohn > ageMark; // Test auf Ungleichheit
+isJohnEqual = ageJohn == ageMark; // Test auf Gleichheit
+
+// Ausgabe
+console.log("Ist John älter als Mark? " + isJohnOlder);
+console.log("Ist John genau so alt wie Mark? " + isJohnEqual);
