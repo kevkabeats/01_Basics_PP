4c09a26c8e68185b1d213c52eab3cc5638984ce6
Kevin Belsch <kev.b95@hotmail.com>
2025-11-07 10:44:34 +0000

    05_Iteration 2

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,10 +6,21 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2025;
+// Berechnung Alter Iteration 1
+/* let year = 2025;
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+ */
+// Berechnung Alter Iteration 2
+let date = new Date();
+let year = date.getFullYear();
+
+console.log("Datum: " + date);
+console.log("Jahr: " + year);
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+//let year = 
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
