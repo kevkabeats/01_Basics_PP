71ccda14be2bd65ab0f64ddcf33519dafe8f8264
Kevin Belsch <kev.b95@hotmail.com>
2025-11-07 08:20:17 +0000

    Updates

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -55,7 +55,7 @@
 /* Variablen vs. Konstanten in JS */
 
 // Variable
-let test; // Variable : Deklaration
+/* let test; // Variable : Deklaration
 test = "hi"; // Wertzuweisung (Initialisierung)
 test = "hello"; // Überschreiben
 console.log("Inhalt: " + test); // Ausgabe 
@@ -63,4 +63,4 @@ console.log("Inhalt: " + test); // Ausgabe
 // Konstante
 const test2 = "Hi!" // Deklaration + Wertzuweisung gemeinsam !
 // test2 = "hello"; //Laufzeit-Fahler 
-console.log("Inhalt:" + test2); //Ausgabe
+console.log("Inhalt:" + test2); //Ausgabe */
