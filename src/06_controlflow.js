be3e25f306bd7b2c763113131685f1f9b8d0ca68
Kevin Belsch <kev.b95@hotmail.com>
2025-11-10 09:18:11 +0000

    if, if - else, if - else if

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -2,18 +2,15 @@
 
 
 // Deklaration + Assignment
-const ageJohn = 25;
-const ageMark = 30;
-
+const ageJohn = 35;
+const ageMark = 40;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
-
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
-
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
@@ -23,21 +20,56 @@ console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------"); */
 
 
+
 /************ IF  ************/
 // TINA --> There is no alternative!
 // entweder JA oder nix ... alternativlos
 // Türsteher-Politik (Doors 10 p.m.)
 
-
 //if(true)                  // Wahrheitswert
 //if(false)
 //if(ageJohn > ageMark)     // direkter Test
-if(isJohnOlder)             // Verweis
+//if(isJohnOlder)             // Verweis
+/* {
+    console.log("John ist älter.");
+} */
+
+
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+/* if (isJohnOlder) 
 {
     console.log("John ist älter.");
-}
+} 
+
 else
 {
-    console.log("John ist nicht älter.");
-}
+   console.log("John ist jünger."); 
+} */
 
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+if (isJohnOlder) 
+{
+    console.log("John ist älter.");
+} 
+
+else if (isJohnEqual)
+{
+   console.log("John und Mark sind gleich alt."); 
+}   
+
+else
+{
+   console.log("John ist jünger."); 
+}
