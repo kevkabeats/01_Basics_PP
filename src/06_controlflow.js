14180d23e61769a55fe6976e253e0324050de372
Kevin Belsch <kev.b95@hotmail.com>
2025-11-10 08:12:44 +0000

    Updates ++

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,20 +1,41 @@
 // Entscheidungsstrukturen | control flow
 
+
 // Deklaration + Assignment
-const ageJohn = 30;
+const ageJohn = 25;
 const ageMark = 30;
+
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
+
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
+
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
+/* console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+console.log("------------------"); */
 
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+
+//if(true)
+//if(false)
+if(isJohnOlder)
+{
+    console.log("John ist älter.");
+}
+else
+{
+    console.log("John ist nicht älter.");
+}
