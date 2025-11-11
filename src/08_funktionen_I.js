fd74cd1b21b6d474b8487abce865e2880b222a3f
Kevin Belsch <kev.b95@hotmail.com>
2025-11-11 08:31:27 +0000

    Update ++

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,3 +2,32 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+
+// Funktionsaufruf | call
+/* test();
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo, Kevin!");
+} */
+// Es ist sinnvoller zuerst die Funktion zu deklarieren und dann aufzurufen.
+
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// call
+ausgabeNamen();
+
+// Funktion
+function ausgabeNamen()
+{
+    // interne Variable
+    let firstName = "Steffen"
+    console.log("Hallo " + firstName + "!");
+}
+
+console.log(firstName); // SCOPE!!
