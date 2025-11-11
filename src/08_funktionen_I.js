14b8c052abf28e58eb5ecb8ad034ae22951a0934
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-11 09:37:49 +0000

    08 / 2c. Mehrere Parameter / Argumente

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -31,10 +31,25 @@ function ausgabeNamen() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamen2("Kevin"); // Argument --> Daten für Parameter
-ausgabeNamen2("Klaus");
-ausgabeNamen2("Ibrahim");
+// ausgabeNamen2("Kevin"); // Argument --> Daten für Parameter
+// ausgabeNamen2("Klaus");
+// ausgabeNamen2("Ibrahim");
 
 function ausgabeNamen2(firstName) {  // Parameter
     console.log("Hallo, " + firstName + "!");
+}
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+ausgabeNamen3("Max","Mütze");
+ausgabeNamen3("Maxine","Mützerich");
+
+// Argumente werden von prompt() geliefert
+const prompt = require('prompt-sync')({sigint: true});
+ausgabeNamen3(prompt("Vorname: "),prompt("Name: "));
+
+
+function ausgabeNamen3(firstName, familyName) {  // Parameter
+    console.log("Hallo, " + firstName + " " + familyName + "!");
 }
