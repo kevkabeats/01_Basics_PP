f0b8e0e020de4e14c2a8da005977a0f6e8185b3c
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-13 09:59:37 +0000

    11 /  Theorie: Array

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -6,6 +6,23 @@
  * 
  */
 
+/*** 02. Theorie: Array */
+
+let arr;
+// arr = new Array(); // Konstruktor
+arr = [];
+arr = [2,11,7,8,10,20];
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Pos. im Array)
+output(arr[arr.length-1]); // letze Pos. / allgemein
+
+
+
+
+
+
 
 
 /**
@@ -18,13 +35,15 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;
